e09839753ce9d99148d3349510717f6310cef36d
JakeTylerSimon <jtsimonbaseball@gmail.com>
2022-08-24 13:41:29 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import {Dimmer, Loader, Container} from 'semantic-ui-react'
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets';
+import Starships from './components/Starships';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [starships, setStarships] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results);
+    setLoading(false);
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results);
+    setLoading(false);
+    }
+    
+    async function fetchStarships() {
+      let res = await fetch('https://swapi.dev/api/starships/?format=json');
+      let data = await res.json();
+      setStarships(data.results);
+    setLoading(false);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+    fetchStarships();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Container>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading...</Loader>
+            </Dimmer>
+          ) : (
+            <Routes>
+              <Route path= '/' element={<Home />} />
+              <Route path='/people' element={<People data={people} />} />
+              <Route path='/planets' element={<Planets data={planets} />} />
+              <Route path='/starships' element={<Starships data={starships} />} />
+            </Routes>
+          )}
+          
+        </Container>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Starships.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Card, Grid} from 'semantic-ui-react'
+
+export default function Starships({ data }) {
+
+  return (
+    <>
+        <h1>People</h1>
+        <Grid columns={3}>
+            {data.map((starships, i) => {
+                return(
+                    <Grid.Column key={i}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>{starships.name}</Card.Header>
+                                <Card.Description>
+                                    <strong>Model</strong>
+                                    <p>{starships.model}</p>
+                                    <strong>Manufacturer</strong>
+                                    <p>{starships.manufacturer}</p>
+                                    <strong>Max Speed</strong>
+                                    <p>{starships.max_atmosphering_speed}kph</p>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                )
+            })}
+        </Grid>
+    </>
+  )
+}
